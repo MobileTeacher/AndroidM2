8d42192cfab3d0ae67b898ebc95a12c5635c0421
Hallison Paz <hallison.dapaz@gmail.com>
2019-09-14 10:37:57 -0300

    checking answers

--- SabeNada/app/src/main/java/br/com/programacaodinamica/sabedenada/MainActivity.kt
+++ SabeNada/app/src/main/java/br/com/programacaodinamica/sabedenada/MainActivity.kt
@@ -8,17 +8,54 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     val questions = listOf(question1, question2)
+    var currentIndex = 0
+    var selectedOption = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        // Configura uma pergunta na tela
+        val currentQuestion = questions[currentIndex]
+        question_textview.text = currentQuestion.text
+        option0_radiobutton.text = currentQuestion.options[0]
+        option1_radiobutton.text = currentQuestion.options[1]
+        option2_radiobutton.text = currentQuestion.options[2]
+        option3_radiobutton.text = currentQuestion.options[3]
+
+
         // adiciona uma função ao botão
         confirmation_button.setOnClickListener {
-            Toast.makeText(this,
-                "Foi Clicado!",
-                Toast.LENGTH_LONG).show()
+
+            selectedOption = when(radiogroup.checkedRadioButtonId){
+                R.id.option0_radiobutton -> 0
+                R.id.option1_radiobutton -> 1
+                R.id.option2_radiobutton -> 2
+                R.id.option3_radiobutton -> 3
+                else -> -1
+            }
+
+            if (selectedOption != -1){
+                if (selectedOption == currentQuestion.answer){
+                    Toast.makeText(this,
+                        "Parabéns, você acertou!",
+                        Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this,
+                        "ERROU!!!!",
+                        Toast.LENGTH_LONG).show()
+                }
+            } else {
+                Toast.makeText(this,
+                    "Selecione uma alternativa!",
+                    Toast.LENGTH_LONG).show()
+            }
+
         }
 
     }
+
+
+
 }
--- SabeNada/app/src/main/java/br/com/programacaodinamica/sabedenada/Question.kt
+++ SabeNada/app/src/main/java/br/com/programacaodinamica/sabedenada/Question.kt
@@ -5,10 +5,14 @@ class Question(val text: String,
                val answer: Int)
 
 
-val question1 = Question("Qual timer está no topo?",
-    listOf("Santos", "Internacional", "Flamengo", "Santa Cruz"),
-    2)
+val question1 = Question("Quantos são os sólidos de Platão?",
+    listOf("5", "10", "15", "20"),
+    0)
 
 val question2 = Question("Em que ano foi proclamada a República brasileira?",
     listOf("1822", "1888", "1860", "1889"),
     3)
+
+val question3 = Question("Qual timer está no topo?",
+    listOf("Santos", "Internacional", "Flamengo", "Santa Cruz"),
+    2)
