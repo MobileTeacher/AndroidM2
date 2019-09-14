537e6639ccc24a6749471cf3e4dad6006939ebb2
Hallison Paz <hallison.dapaz@gmail.com>
2019-09-14 14:19:00 -0300

    getting question atttributes from form

--- SabeNada/app/src/main/java/br/com/programacaodinamica/sabedenada/AddQuestionActivity.kt
+++ SabeNada/app/src/main/java/br/com/programacaodinamica/sabedenada/AddQuestionActivity.kt
@@ -16,10 +16,45 @@ class AddQuestionActivity : AppCompatActivity() {
                             "Programação",
                             "Entretenimento")
 
+    var category = categories[0]
+    var text = ""
+    // lista mutável
+    var options = mutableListOf("", "", "", "")
+    var answer = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_question)
 
+        setUpSpinner()
+        setUpListeners()
+
+    }
+
+    fun setUpListeners(){
+        // configurar a ação do botão
+        save_button.setOnClickListener {
+            text = question_input.text.toString()
+            options[0] = option0_edittext.text.toString()
+            options[1] = option1_edittext.text.toString()
+            options[2] = option2_edittext.text.toString()
+            options[3] = option3_edittext.text.toString()
+            answer = when(choices_radiogroup.checkedRadioButtonId){
+                R.id.choice0_radiobutton -> 0
+                R.id.choice1_radiobutton -> 1
+                R.id.choice2_radiobutton -> 2
+                R.id.choice3_radiobutton -> 3
+                else -> -1
+            }
+            // próximo passo, encerrar formulário e construir pergunta
+            val question = Question(text, options, answer, category)
+            random_question_textview.text = "$question" //question.toString()
+
+        }
+    }
+
+
+    fun setUpSpinner(){
         val adapter = ArrayAdapter<String>(this,
             android.R.layout.simple_spinner_item,
             categories)
@@ -29,17 +64,12 @@ class AddQuestionActivity : AppCompatActivity() {
         category_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             override fun onNothingSelected(p0: AdapterView<*>?) {}
 
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                Toast.makeText(this@AddQuestionActivity,
-                    "Selecionou ${categories[p2]}",
-                    Toast.LENGTH_LONG
-                    ).show()
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, index: Int, p3: Long) {
+                // guarda o valor da categoria selecionada no Spinner
+                category = categories[index]
             }
 
         }
-
     }
-
-
 
 }
--- SabeNada/app/src/main/java/br/com/programacaodinamica/sabedenada/Question.kt
+++ SabeNada/app/src/main/java/br/com/programacaodinamica/sabedenada/Question.kt
@@ -2,17 +2,23 @@ package br.com.programacaodinamica.sabedenada
 
 class Question(val text: String,
                val options: List<String>,
-               val answer: Int)
+               val answer: Int,
+               val category: String){
+
+    override fun toString(): String {
+        return "$text\n${options.joinToString("\n")}\n$answer\n$category"
+    }
+}
 
 
 val question1 = Question("Quantos são os sólidos de Platão?",
     listOf("5", "10", "15", "20"),
-    0)
+    0, "Matemática")
 
 val question2 = Question("Em que ano foi proclamada a República brasileira?",
     listOf("1822", "1888", "1860", "1889"),
-    3)
+    3, "História")
 
 val question3 = Question("Qual timer está no topo?",
     listOf("Santos", "Internacional", "Flamengo", "Santa Cruz"),
-    2)
+    2, "Esportes")
