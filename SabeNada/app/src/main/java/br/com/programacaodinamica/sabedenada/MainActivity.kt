28accfd55292632822470eac95a84bcdfbe42699
Hallison Paz <hallison.dapaz@gmail.com>
2019-09-14 11:06:06 -0300

    advancing to next question

--- SabeNada/app/src/main/java/br/com/programacaodinamica/sabedenada/MainActivity.kt
+++ SabeNada/app/src/main/java/br/com/programacaodinamica/sabedenada/MainActivity.kt
@@ -7,7 +7,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val questions = listOf(question1, question2)
+    val questions = listOf(question1, question2, question3)
     var currentIndex = 0
     var selectedOption = -1
 
@@ -15,19 +15,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setUpQuestion(currentIndex)
+        setUpListeners()
+    }
 
-        // Configura uma pergunta na tela
+    // Configura uma pergunta na tela
+    fun setUpQuestion(currentIndex: Int){
         val currentQuestion = questions[currentIndex]
         question_textview.text = currentQuestion.text
         option0_radiobutton.text = currentQuestion.options[0]
         option1_radiobutton.text = currentQuestion.options[1]
         option2_radiobutton.text = currentQuestion.options[2]
         option3_radiobutton.text = currentQuestion.options[3]
+    }
 
-
+    // Configura botões e outras views interativas
+    fun setUpListeners(){
         // adiciona uma função ao botão
         confirmation_button.setOnClickListener {
-
+            // verifica qual opção foi seleciona
             selectedOption = when(radiogroup.checkedRadioButtonId){
                 R.id.option0_radiobutton -> 0
                 R.id.option1_radiobutton -> 1
@@ -36,11 +42,21 @@ class MainActivity : AppCompatActivity() {
                 else -> -1
             }
 
-            if (selectedOption != -1){
+            if (selectedOption != -1){ //se não é uma opção inválida
+                val currentQuestion = questions[currentIndex]
                 if (selectedOption == currentQuestion.answer){
                     Toast.makeText(this,
                         "Parabéns, você acertou!",
                         Toast.LENGTH_LONG).show()
+
+                    // apaga opção selecionada
+                    radiogroup.clearCheck()
+                    // avança para próxima pergunta
+                    currentIndex = (currentIndex + 1)%questions.size
+                    setUpQuestion(currentIndex)
+
+
+
                 } else {
                     Toast.makeText(this,
                         "ERROU!!!!",
@@ -53,7 +69,6 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
-
     }
 
 
