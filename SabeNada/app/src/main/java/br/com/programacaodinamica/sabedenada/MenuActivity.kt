8ccd8add96d9c0b91d6efadc2fb0747e4c3ee71b
Hallison Paz <hallison.dapaz@gmail.com>
2019-09-14 14:54:17 -0300

    all activities integrated from menu

--- SabeNada/app/src/main/java/br/com/programacaodinamica/sabedenada/MenuActivity.kt
+++ SabeNada/app/src/main/java/br/com/programacaodinamica/sabedenada/MenuActivity.kt
@@ -1,12 +1,38 @@
 package br.com.programacaodinamica.sabedenada
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        setUpListeners()
+    }
+
+    fun setUpListeners(){
+
+
+        play_button.setOnClickListener {
+            // Ir para tela de "Jogo do Quiz"
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        list_button.setOnClickListener {
+            // Ir para tela de "Lista de Perguntas"
+            val intent = Intent(this, QuestionListActivity::class.java)
+            startActivity(intent)
+        }
+
+        add_button.setOnClickListener {
+            // Ir para tela de "Adicionar Pergunta"
+            startActivity(Intent(this, AddQuestionActivity::class.java))
+        }
+
     }
 }
